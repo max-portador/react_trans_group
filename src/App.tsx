139b5446eb7fef9c00eb33dd4372f61d2a53fe30
max-portador <46213974+max-portador@users.noreply.github.com>
2022-06-06 23:26:43 +0300

    in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,59 @@
-import React, {useEffect, useRef, useState} from 'react';
-import cn from 'classnames';
+import React, {FC} from 'react';
 import './App.css';
-import {CSSTransition} from "react-transition-group";
-
-const steps = [60, 20, 120, 100, 25, 0]
-    .map((step, i) => ({
-        label: `Step ${i + 1}`,
-        dist: step
-    }))
-
-const getTotalDist = (curr: number) => {
-    return steps.reduce((acc, step, i) => {
-        if (i < curr) {
-            return acc + step.dist + (i ? 60 : 30)
-        } else {
-            return acc
-        }
-    }, 10)
-}
+import styled from "styled-components";
+import {useInView} from "react-intersection-observer";
 
 function App() {
-    const [isVisible, setIsVisible] = useState<boolean>(false)
-    const [active, setActive] = useState<number>(0)
-    const ref = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const foo = () => {console.log('transitionrun fired')}
-        if (ref.current){
-            ref.current.addEventListener('transitionrun', foo);
-        }
-
-        return () => {
-            if (ref.current)
-            ref.current.removeEventListener('transitionrun', foo);
-        }
-    }, [ref])
-
-    const clickHandler = function(index: number){
-        setActive(index)
-    }
+    const cards = ['First Card',
+        ...Array(25).fill('Card'),
+        'Last Card',
+    ]
 
 
+
+
+    // @ts-ignore
     return (
         <div className="App">
-            <button onClick={() => {
-                setIsVisible(prevState => !prevState)
-            }}>
-                {isVisible ? 'hide' : 'show'}
-            </button>
-            <h2>{getTotalDist(active)}</h2>
-            <div className="wrapper">
-                <div className="container">
-                    <div className="scale"
-                         ref={ref}
-                         style={{
-                             width: getTotalDist(active),
-                             transition: 'width .3s linear'
-                         }}
-                    />
-
-                    <ul className="progressbar">
-                        {
-                            steps.map((step, i) => (
-                                <>
-                                    <li
-                                        key={step.label}
-                                        className={cn('li',
-                                            {
-                                                'active': i <= active,
-                                                'preactive': i < active
-                                            })
-
-                                        }
-                                        style={{
-                                            marginRight: i === steps.length - 1 ? 0 : step.dist
-                                        }}
-                                        onClick={() => clickHandler(i)}
-
-                                    >
-                                        {step.label}
-                                    </li>
-                                </>
-
-                            ))
-                        }
-                    </ul>
-                </div>
-                <CSSTransition
-                    in={isVisible}
-                    timeout={500}
-                    classNames={{
-                        enterActive: 'circle-show',
-                        enterDone: 'circle-rotate',
-                        exitActive: 'circle-hide',
-                    }}
-                    mountOnEnter
-                    unmountOnExit
-                >
-                    <div className="circle"/>
-                </CSSTransition>
-            </div>
-
+            <CardContainer>
+                {cards.map( text =>  <Card text={text}/>)}
+            </CardContainer>
         </div>
     );
 }
 
 export default App;
+
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 100px;
+  gap: 1rem;
+  height: 250vh;
+  align-items: flex-start;
+`
+
+const Card:FC<{text: string}> = ({text}) => {
+
+    const { ref, inView } = useInView({
+        threshold: .1
+    });
+
+    return <StDiv ref={ref} isShown={inView}>{text}</StDiv>
+}
+
+
+
+const StDiv = styled.div<{isShown?: boolean}>`
+  background-color: steelblue;
+  border: 1px solid black;
+  border-radius: .25rem;
+  padding: .5rem;
+  height: 150px;
+  width: 25vw;
+  transform: ${props => props.isShown ? 'translateX(0)' : 'translateX(100px)'} ;
+  opacity: ${props => props.isShown ? 1 : 0} ;
+  transition: 150ms;
+`
